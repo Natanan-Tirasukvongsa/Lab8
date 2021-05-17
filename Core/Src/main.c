6e5b2a8571ee58450d4327ff69cec28b2f503a56
Natanan-Tirasukvongsa <78638430+Natanan-Tirasukvongsa@users.noreply.github.com>
2021-05-18 02:37:41 +0700

    change clock

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -282,13 +282,12 @@ int main(void)
 	  		  				STATE_Display = StateDisplay_Menu1_Print; //เปลี่ยน state เป็น หน้า menu1
 	  		  				break;
 	  		  		}
-
-	  		  	if (micros() - timestamp >= LED_TOGGLE_DELAY/frequency && on ==1)
-	  		  	{
-	  		  		timestamp = micros();
-	  		  		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-	  		  	}
 	  		}
+			if (micros() - timestamp >= LED_TOGGLE_DELAY/frequency && on ==1)
+			{
+				timestamp = micros();
+				HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+			}
 
 	  }
   }
@@ -316,9 +315,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -333,7 +332,7 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
   {
     Error_Handler();
   }
